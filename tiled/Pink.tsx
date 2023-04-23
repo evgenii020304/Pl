f93464b755e4490f6c27120254a167e8845dd7ed
Evgeniy <Evgeniy@EVGENIYCOMPUTER>
2023-04-23 18:22:41 +0300

    Добавьте файлы проекта.

---
+++ tiled/Pink.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Pink" tilewidth="32" tileheight="30" tilecount="4" columns="2">
+ <image source="back/Free/Background/Pink.png" width="64" height="64"/>
+</tileset>
